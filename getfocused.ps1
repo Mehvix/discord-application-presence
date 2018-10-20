bc81545bff4daa4a36b2ad445a870ad9bd683a1c
Mehvix <maxlangdonvogel@gmail.com>
2018-10-20 13:16:08 -0500

    Initial commit

---
+++ getfocused.ps1
@@ -0,0 +1,9 @@
+$code = @'
+    [DllImport("user32.dll")]
+     public static extern IntPtr GetForegroundWindow();
+'@
+Add-Type $code -Name Utils -Namespace Win32
+$hwnd = [Win32.Utils]::GetForegroundWindow()
+Get-Process |
+    Where-Object { $_.mainWindowHandle -eq $hwnd } |
+    Select-Object processName, MainWindowTItle, MainWindowHandle
